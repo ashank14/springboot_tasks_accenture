092e1386df65404cff4efce6b09861a1fb87a543
Ashank14 <ashanksethi@gmail.com>
2025-06-16 00:31:44 +0530

    final

--- src/main/java/spring_tasks/spring_project/kafka/consumer/KafkaConsumerService.java
+++ src/main/java/spring_tasks/spring_project/kafka/consumer/KafkaConsumerService.java
@@ -3,12 +3,14 @@ package spring_tasks.spring_project.kafka.consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumerService {
     private static final Logger logger = LoggerFactory.getLogger(KafkaConsumerService.class);
+
 
     @KafkaListener(topics = "book-notification", groupId = "book-group")
     public void listen(String message) {
--- src/main/java/spring_tasks/spring_project/kafka/producer/KafkaProducerService.java
+++ src/main/java/spring_tasks/spring_project/kafka/producer/KafkaProducerService.java
@@ -4,6 +4,7 @@ package spring_tasks.spring_project.kafka.producer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +15,11 @@ public class KafkaProducerService {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    @Value("${spring.kafka.topic.name}")
+    private String topic;
+
     public void sendNotification(String message) {
         logger.info("Sending notification: {}", message);
-        kafkaTemplate.send("book-notification", message);
+        kafkaTemplate.send(topic, message);
     }
 }
